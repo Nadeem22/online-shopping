4c4843e468d895270163c79c811f6bb5affc7283
nadeem <nadeem2107@gmail.com>
2018-05-04 12:22:24 +0530

    Menu Linking Successfully Done

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,13 +1,19 @@
 package net.kzn.onlineshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import net.kzn.shoppingbackend.dao.CategoryDAO;
+import net.kzn.shoppingbackend.dto.Category;
 
 @Controller
 public class PageController {
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
 	
 	@RequestMapping(value= {"/", "/home", "/index"})
 	
@@ -15,6 +21,11 @@ public class PageController {
 		
 		ModelAndView mv= new ModelAndView("page");
 		mv.addObject("title","HOME");
+		
+		//passing the list of category
+		
+		mv.addObject("categories", categoryDAO.list());
+		
 		mv.addObject("userClickHome", true);
 		return mv;
 	}
@@ -60,5 +71,55 @@ public ModelAndView contact() {
 	mv.addObject("userClickContact", true);
 	return mv;
 }
+
+/*
+ * Methods to load All products and based on category
+ * 
+ * */
+
+@RequestMapping(value= "show/all/products")
+
+public ModelAndView showAllProducts() {
+	
+	ModelAndView mv= new ModelAndView("page");
+	mv.addObject("title","All Products");
+	
+	//passing the list of category
+	
+	mv.addObject("categories", categoryDAO.list());
+	
+	mv.addObject("userClickAllProducts", true);
+	return mv;
+}
+
+@RequestMapping(value= "show/category/{id}/products")
+
+public ModelAndView showCategoryProducts(@PathVariable("id") int id) {
+	
+	ModelAndView mv= new ModelAndView("page");
+	
+	
+	//category DAO to fetch a single Category
+	
+	Category category=null;
+	category=categoryDAO.get(id);
+	
+	
+	
+	mv.addObject("title", category.getName());
+	
+	//passing the list of category
+	
+	mv.addObject("categories", categoryDAO.list());
+	
+	//passing the single category object
+	
+	mv.addObject("category", category);
+	
+	mv.addObject("userClickCategoryProducts", true);
+	return mv;
+}
+
+
 
 }
--- onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
@@ -70,6 +70,12 @@
 			<c:if test="${userClickContact==true }">
 				<%@include file="contact.jsp"%>
 			</c:if>
+			
+			<!-- Loading When Click Contact -->
+
+			<c:if test="${userClickAllProducts==true or userClickCategoryProducts==true}">
+				<%@include file="listProducts.jsp"%>
+			</c:if>
 
 			<!-- /.container -->
 			
